73819a540acbc39b5667f2d172e9ecd765f7bf28
ngocminhk65 <20020061@vnu.edu.vn>
2023-10-26 16:41:03 +0700

    reset commit
    fix wrong commit error

--- backend/src/assigment/assigment.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AssigmentService } from './assigment.service';
-import { AssigmentController } from './assigment.controller';
-
-@Module({
-  controllers: [AssigmentController],
-  providers: [AssigmentService],
-})
-export class AssigmentModule {}
--- backend/src/assigment/dto/update-assigment.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAssigmentDto } from './create-assigment.dto';
-
-export class UpdateAssigmentDto extends PartialType(CreateAssigmentDto) {}
